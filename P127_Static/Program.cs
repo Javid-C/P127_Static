78357f53b08a0be34b84d9cd8ced9114ead3978d
Javid-C <javidia@code.edu.az>
2022-02-23 12:41:37 +0400

    Initial commit

---
+++ P127_Static/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P127_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student;
+            //Student student1 = new Student("Oruc", "Dostuyev");
+            //Student student2 = new Student("Oruc", "Dostuyev");
+            Console.WriteLine(Student.count);
+        }
+    }
+}
---
+++ P127_Static/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P127_Static
+{
+    class Student
+    {
+        public int Id;
+        public static int count=5;
+        public static int total;
+        public string Name;
+        public string Surname;
+
+        public Student(string name,string surname)
+        {
+            Console.WriteLine("normal");
+            count++;
+            Name = name;
+            Surname = surname;
+            Id = count;
+        }
+
+        static Student()
+        {
+            Console.WriteLine("Static");
+        }
+
+
+        public static int Calc(Student stu)
+        {
+            return count + stu.Id;
+        }
+
+        
+    }
+}
